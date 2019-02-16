85902338618371fd6e72f420b1e44850cfb5c3b0
Mitch Nenn <mitch.nenn@earthlink.net>
2019-02-15 20:45:08 -0800

    Find a filename in a path.

---
+++ RegexLib/FileRegex.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexLib
+{
+    public class FileRegex : Regex
+    {
+        public FileRegex() 
+            : base(@"\w+:\\(\w+\\)*(?<file>\w*\.\w*)", RegexOptions.Compiled | RegexOptions.IgnoreCase)
+        {
+            
+        }
+
+        public string GetFileName(string fullPath)
+        {
+            return Match(fullPath).Groups["file"].Value;
+        }
+    }
+}
---
+++ RegexLibTest/FileRegex.cs
@@ -0,0 +1,31 @@
+using System;
+using RegexLib;
+using Xunit;
+
+namespace RegexLibTest
+{
+    public class FileRegexTest
+    {
+        [Fact]
+        public void Create_File_Regex()
+        {
+            var obj = new FileRegex();
+            Assert.NotNull(obj);
+        }
+
+        [Fact]
+        public void Get_Filename_From_Path()
+        {
+            // Arrange.
+            var regext = new FileRegex();
+            var filename = "myfile.json";
+            var path = $"C:\\this\\is\\a\\path\\{filename}";
+
+            // Act.
+            var actual = regext.GetFileName(path);
+
+            // Assert.
+            Assert.Equal(filename, actual);
+        }
+    }
+}
